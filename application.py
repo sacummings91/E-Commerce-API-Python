6ae8bdb9d1e147491334df44a09eaaceb7e82b50
sacummings91 <sacummings91@gmail.com>
2017-12-19 17:48:18 -0800

    minor change

--- application.py
+++ application.py
@@ -15,8 +15,8 @@ import os
 
 application = Flask(__name__)
 CORS(application)
-application.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ecommerceadmin:therealst33zy!!!@aazr52go8cnf2a.cxbnexrfclol.us-west-1.rds.amazonaws.com/capstone_dev'
-# application.config['SQLALCHEMY_DATABASE_URI'] = os.environ['SQLALCHEMY_DATABASE_URI']
+# application.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ecommerceadmin:therealst33zy!!!@aazr52go8cnf2a.cxbnexrfclol.us-west-1.rds.amazonaws.com/capstone_dev'
+application.config['SQLALCHEMY_DATABASE_URI'] = os.environ['SQLALCHEMY_DATABASE_URI']
 application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 application.config['JWT_EXPIRATION_DELTA'] = datetime.timedelta(days=7)
 application.secret_key = 'h29fh09x9fha9w02h'
